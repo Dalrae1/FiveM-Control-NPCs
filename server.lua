ffeee96fcfc06abe7da8327deb184c2b8ded4d62
Dalrae1 <52721173+Dalrae1@users.noreply.github.com>
2021-07-19 18:52:35 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,17 @@
+local Config = {}
+Config.UsePermissions = false -- Set to false to allow everyone to use the script
+
 RegisterServerEvent("DalraeTakeControl:DeleteEntity", function(pedNetID)
     local ped = NetworkGetEntityFromNetworkId(pedNetID)
     if DoesEntityExist(ped) then
         DeleteEntity(ped)
+    end
+end)
+
+RegisterServerEvent("DalraeTakeControl:SendPermissions", function()
+    if Config.UsePermissions then
+        TriggerClientEvent("DalraeTakeControl:RecievePermissions", source, IsPlayerAceAllowed(source, "ControlNPCs.Allow"))
+    else
+        TriggerClientEvent("DalraeTakeControl:RecievePermissions", source, true)
     end
 end)
